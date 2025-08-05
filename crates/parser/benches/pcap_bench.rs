3ea05012c5cba750cf4e14436a8965c32eda8c98
sankooc <sankooc@163.com>
2025-08-05 10:40:46 +0800

    Add theme and color management features
    - Introduced new JSON file for theme mapping (mapper.json).
    - Added a comprehensive theme list (theme.list) containing various design tokens.
    - Implemented color management in the frame component using tinycolor2 for dynamic color adjustments.
    - Refactored conversation and connection list components to improve type definitions and structure.
    - Cleaned up unused code and commented-out sections in common.ts and other files.
    - Enhanced styling in frame and HTTP components by adjusting font weights and styles.
    - Updated theme building logic to dynamically apply CSS variables based on the current theme.

--- crates/parser/benches/pcap_bench.rs
+++ crates/parser/benches/pcap_bench.rs
@@ -1,15 +1,32 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use pcap::common::Instance;
+// use criterion::{criterion_group, criterion_main, Criterion};
+// use pcap::common::Instance;
 
-fn bench_parse_pcap(c: &mut Criterion) {
-    let data = std::fs::read("../../../pcaps/11.pcapng").unwrap();
-    c.bench_function("parse_pcap", |b| {
-        b.iter(|| {
-            let mut instance = Instance::new(65536);
-            instance.update(data.clone()).unwrap();
-        })
-    });
+// fn bench_parse_pcap(c: &mut Criterion) {
+//     let data = std::fs::read("../../../pcaps/11.pcapng").unwrap();
+//     c.bench_function("parse_pcap", |b| {
+//         b.iter(|| {
+//             let mut instance = Instance::new(65536);
+//             instance.update(data.clone()).unwrap();
+//         })
+//     });
+// }
+
+// criterion_group!(benches, bench_parse_pcap);
+// criterion_main!(benches);
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n - 1) + fibonacci(n - 2),
+    }
 }
 
-criterion_group!(benches, bench_parse_pcap);
+fn bench_fib(c: &mut Criterion) {
+    c.bench_function("fib 10", |b| b.iter(|| fibonacci(black_box(10))));
+}
+
+criterion_group!(benches, bench_fib);
 criterion_main!(benches);
